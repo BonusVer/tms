f4b1b651662edbdb5623d449b5b1bccc99f4b516
Vitaly <minsvital@mail.ru>
2025-03-31 21:02:57 +0400

    Создание проекта - Написание Сервисов, контроллеров и репозиториев

---
+++ task-service/src/main/java/com/bonusver/task/service/DefaultTaskService.java
@@ -0,0 +1,10 @@
+package com.bonusver.task.service;
+
+import com.bonusver.task.entity.Task;
+
+public class DefaultTaskService implements TaskService{
+    @Override
+    public Iterable<Task> getAllTasks() {
+        return null;
+    }
+}
