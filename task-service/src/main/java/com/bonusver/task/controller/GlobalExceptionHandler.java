1b4dbd90985b207404bc53e947eddeadaccc3c6f
Vitaly <minsvital@mail.ru>
2025-04-15 22:02:59 +0400

    Создание проекта - Написание Сервисов, контроллеров и репозиториев

---
+++ task-service/src/main/java/com/bonusver/task/controller/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.bonusver.task.controller;
+
+import com.bonusver.task.controller.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
