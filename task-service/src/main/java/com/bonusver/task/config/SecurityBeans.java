db9c743e20237f03258c48028524236f17a0f9a5
Vitaly <minsvital@mail.ru>
2025-04-15 22:03:50 +0400

    Создание проекта - Написание Сервисов, контроллеров и репозиториев

---
+++ task-service/src/main/java/com/bonusver/task/config/SecurityBeans.java
@@ -0,0 +1,22 @@
+package com.bonusver.task.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+public class SecurityBeans {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
