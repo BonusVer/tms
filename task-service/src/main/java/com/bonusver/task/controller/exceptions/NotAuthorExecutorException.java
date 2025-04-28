d665007c38c39002005b979a33454335dac50566
Vitaly <minsvital@mail.ru>
2025-04-28 17:55:13 +0400

    Создание проекта - Написание Сервисов, контроллеров и репозиториев

---
+++ task-service/src/main/java/com/bonusver/task/controller/exceptions/NotAuthorExecutorException.java
@@ -0,0 +1,8 @@
+package com.bonusver.task.controller.exceptions;
+
+
+public class NotAuthorExecutorException extends RuntimeException {
+    public NotAuthorExecutorException(String message) {
+        super(message);
+    }
+}
