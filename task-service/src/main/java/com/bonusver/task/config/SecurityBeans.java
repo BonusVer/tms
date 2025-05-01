bc6848ce8c006e99b404ea22e2e9ea2722888d8f
Vitaly <minsvital@mail.ru>
2025-05-01 14:42:06 +0400

    Создание проекта - Написание Сервисов, контроллеров и репозиториев

--- task-service/src/main/java/com/bonusver/task/config/SecurityBeans.java
+++ task-service/src/main/java/com/bonusver/task/config/SecurityBeans.java
@@ -3,13 +3,10 @@ package com.bonusver.task.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -17,12 +14,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityBeans {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->authorizeRequests
-                        .requestMatchers(HttpMethod.GET, "/api/v1/tasks").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/tasks").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/v1/tasks").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/tasks/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/v1/tasks/{taskId}/comments").hasAnyRole("USER","ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/tasks/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/tasks/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/tasks/by-author/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/tasks/by-executor/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().denyAll())
 
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
